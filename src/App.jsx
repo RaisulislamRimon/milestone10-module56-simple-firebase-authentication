c60cd9861d84427b5ad12f354dc2cbbd2d573490
MD. Raisul Islam Rimon <rimonppip0@gmail.com>
2022-10-13 18:55:44 +0600

    added github login system

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,19 @@ import {
   signInWithPopup,
   GoogleAuthProvider,
   signOut,
+  GithubAuthProvider,
 } from "firebase/auth";
 import app from "./firebaseConfig/firebaseConfig";
 
 const App = () => {
   const [user, setUser] = React.useState({});
 
-  const provider = new GoogleAuthProvider();
+  const googleProvider = new GoogleAuthProvider();
+  const githubProvider = new GithubAuthProvider();
+
   const auth = getAuth(app);
   const handleGoogleSignIn = () => {
-    signInWithPopup(auth, provider)
+    signInWithPopup(auth, googleProvider)
       .then((result) => {
         // This gives you a Google Access Token. You can use it to access the Google API.
         const credential = GoogleAuthProvider.credentialFromResult(result);
@@ -38,7 +41,7 @@ const App = () => {
       });
   };
 
-  const handleSignOut = () => {
+  const handleGoogleSignOut = () => {
     signOut(auth)
       .then(() => {
         setUser({});
@@ -47,11 +50,33 @@ const App = () => {
         console.log(error);
       });
   };
+  const handleGithubLogin = () => {
+    signInWithPopup(auth, githubProvider)
+      .then((result) => {
+        // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+        const credential = GithubAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+
+        // The signed-in user info.
+        const user = result.user;
+        // ...
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GithubAuthProvider.credentialFromError(error);
+        // ...
+      });
+  };
   return (
     <div className="App">
       {user.email ? (
         <div>
-          <button onClick={handleSignOut}>Sign out</button>
+          <button onClick={handleGoogleSignOut}>Sign out</button>
           <h1>{user.displayName}</h1>
           <p>{user.email}</p>
           <img src={user.photoURL} alt={user.displayName} />
@@ -59,6 +84,7 @@ const App = () => {
       ) : (
         <button onClick={handleGoogleSignIn}>Google sign in</button>
       )}
+      <button onClick={handleGithubLogin}>Github login</button>
     </div>
   );
 };
